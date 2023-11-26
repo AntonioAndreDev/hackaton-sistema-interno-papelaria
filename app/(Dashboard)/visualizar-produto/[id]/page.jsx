ec48a73554186e55c98f7e59c51b4b1cc4d24592
Antonio Andre <antonioandre1008@gmail.com>
2023-11-26 14:00:56 -0300

    correcao na centralizacao

--- app/(Dashboard)/visualizar-produto/[id]/page.jsx
+++ app/(Dashboard)/visualizar-produto/[id]/page.jsx
@@ -25,7 +25,7 @@ export default function VisualizarProduto({ params }) {
   console.log(produto);
 
   return (
-    <main className="mt-12 mx-auto flex flex-col">
+    <main className="mt-12 items-center w-full flex flex-col">
       <h1 className="text-center text-xl mb-4">
         {carregando ? (
           <Skeleton className="w-[400px] rounded-lg">
