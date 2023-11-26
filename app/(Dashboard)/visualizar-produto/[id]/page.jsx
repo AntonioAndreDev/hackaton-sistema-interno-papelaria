fc0006de2be02ae0fe3a003f9b2f3c399e21ee49
Antonio Andre <antonioandre1008@gmail.com>
2023-11-26 13:25:20 -0300

    estilizacao da rota 'visualizar-produto'

--- app/(Dashboard)/visualizar-produto/[id]/page.jsx
+++ app/(Dashboard)/visualizar-produto/[id]/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
 
 export default function VisualizarProduto({ params }) {
   const [produto, setProduto] = useState("");
@@ -13,23 +14,35 @@ export default function VisualizarProduto({ params }) {
   console.log(produto);
 
   return (
-    <main>
-      <img className="w-1/4" src={produto.imagem} alt={`Imagem de ${produto.name}`} />
-      <h1>
-        <strong>Nome:</strong> {produto.name}
-      </h1>
-      <h6>
-        <strong>Estoque:</strong> {produto.estoque}
-      </h6>
-      <h6>
-        <strong>Valor:</strong> R${produto.valor}
-      </h6>
-      <h6>
-        <strong>Tipo:</strong> {produto.tipoDeProduto}
-      </h6>
-      <h6>
-        <strong>Descrição:</strong> {produto.descricao}
-      </h6>
+    <main className="mt-12 mx-auto">
+      <h1 className="text-center text-xl mb-4">Detalhes do Produto: {produto.name}</h1>
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            className="w-16"
+            alt="nextui logo"
+            height={200}
+            width={200}
+            radius="sm"
+            src={produto.imagem}
+          />
+          <div className="flex flex-col">
+            <p className="text-md font-medium">{produto.name}</p>
+            <p className="text-small text-default-500">{produto.tipoDeProduto}</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>{produto.descricao}</p>
+        </CardBody>
+        <CardBody>
+          <p>{produto.estoque} unidades.</p>
+        </CardBody>
+        <CardBody>
+          <p>R${produto.valor}</p>
+        </CardBody>
+        <Divider />
+      </Card>
     </main>
   );
 }
